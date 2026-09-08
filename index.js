b21b974539b11a8f6d1e01f8aac0f39d21d65a7e
melvycastillo-alt <melvy.castillo@gmail.com>
2026-09-08 14:30:43 -0400

    Delete index.js

--- index.js
+++
@@ -1,3 +0,0 @@
-const VERIFY_TOKEN = "merelyn_token_2026"; // Usted inventa esta clave
-const ACCESS_TOKEN = "SU_TOKEN_PERMANENTE_AQUI"; // Pegue aquí el token que acaba de generar
-const PHONE_NUMBER_ID = "1286648534534761"; // El ID que ya tiene
